9b1c2fd339178b3641e40b9cf5a440f9e1d7de81
AmrMohamad <amr.mohamed124124@gmail.com>
2020-10-16 23:03:54 +0200

    create Authentication & validation to Register page

--- LetsChat/Controllers/RegisterViewController.swift
+++ LetsChat/Controllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -20,7 +21,48 @@ class RegisterViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func registerAction(_ sender: Any) {
+        
+        if passwordTextField.text == confirmTextField.text {
+            
+            if let email = emailTextField.text , let password = passwordTextField.text{
+                Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                    if let e = error {
+                        
+                        let alertControllerOK = UIAlertController.init(title: "Someting Wrong", message: e.localizedDescription , preferredStyle: .alert)
+                        
+                        let oKAction = UIAlertAction(title: "Ok", style: .default)
+                        { _ in
+                            self.navigationController?.popToRootViewController(animated: true)
+                        }
+                        
+                        alertControllerOK.addAction(oKAction)
+                        self.present(alertControllerOK, animated: true, completion:  nil)
+                        
+                        print(e.localizedDescription)
+                    } else{
+                        self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                    }
+                }
+            }
+            
+        } else {
+            
+            let alertControllerOK = UIAlertController.init(title: "Someting Wrong", message: "It's not the same password Please,Check your Password again  ", preferredStyle: .alert)
+            
+            let oKAction = UIAlertAction(title: "Ok", style: .default)
+            { _ in
+                self.navigationController?.popToRootViewController(animated: true)
+            }
+            
+            alertControllerOK.addAction(oKAction)
+            self.present(alertControllerOK, animated: true, completion:  nil)
 
+            
+        }
+        
+    }
+    
     /*
     // MARK: - Navigation
 
