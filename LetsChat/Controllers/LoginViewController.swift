00f4678d96f0ddcc77ddb61bf90208090aeebbf2
AmrMohamad <amr.mohamed124124@gmail.com>
2020-10-17 12:25:18 +0200

    add loginVC to LogInSB

---
+++ LetsChat/Controllers/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  LetsChat
+//
+//  Created by Amr Mohamad on 10/17/20.
+//  Copyright © 2020 Amr Mohamad. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
