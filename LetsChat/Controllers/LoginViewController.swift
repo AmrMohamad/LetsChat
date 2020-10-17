af1ea9ce42239030c95f213d93c1af301fac102e
AmrMohamad <amr.mohamed124124@gmail.com>
2020-10-17 12:44:18 +0200

    connect login to firebase and create a validation(alert message)

--- LetsChat/Controllers/LoginViewController.swift
+++ LetsChat/Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -18,7 +19,32 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func loginAction(_ sender: UIButton) {
+        if let email = emailTextField.text , let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    let alertControllerOK = UIAlertController.init(title: "Someting Wrong", message: e.localizedDescription , preferredStyle: .alert)
+                    
+                    let oKAction = UIAlertAction(title: "Ok", style: .default)
+                    { _ in
+                       // self.navigationController?.popToRootViewController(animated: true)
+                    }
+                    
+                    alertControllerOK.addAction(oKAction)
+                    self.present(alertControllerOK, animated: true, completion:  nil)
+                    
+                    print(e)
+                } else{
+                    let storyboard = UIStoryboard(name: "Chat", bundle: nil)
+                    let vc = storyboard.instantiateViewController(withIdentifier: "ChatViewController")
+                    vc.modalPresentationStyle = .fullScreen
+                    self.navigationController?.pushViewController(vc, animated: true)
+                    self.navigationController?.isNavigationBarHidden = true
+                }
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
