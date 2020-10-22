1ef640e76477a18ba302b3e20ebd13aeaa0586b2
AmrMohamad <amr.mohamed124124@gmail.com>
2020-10-23 00:00:16 +0200

    add Message model

---
+++ LetsChat/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  LetsChat
+//
+//  Created by Amr Mohamad on 10/22/20.
+//  Copyright © 2020 Amr Mohamad. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
