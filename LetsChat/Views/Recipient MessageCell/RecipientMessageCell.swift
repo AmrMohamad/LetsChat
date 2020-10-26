0143e72e75524aa2b0a549f366e607ab064c8b13
AmrMohamad <amr.mohamed124124@gmail.com>
2020-10-26 16:59:58 +0200

    [Fix] the size of message shape of RecipientMessageCell

--- LetsChat/Views/Recipient MessageCell/RecipientMessageCell.swift
+++ LetsChat/Views/Recipient MessageCell/RecipientMessageCell.swift
@@ -18,7 +18,7 @@ class RecipientMessageCell: UITableViewCell {
         
         messageShape.layer.cornerRadius = messageShape.frame.size.height / 6
         //messageShape.layer.shadowColor = CGColor.init(srgbRed: 0.980, green: 0.965, blue: 0.996, alpha: 1.0)
-        messageShape.layer.shadowColor = CGColor.init(srgbRed: 0.478, green: 0.255, blue: 0.718, alpha: 1.0)
+        messageShape.layer.shadowColor = CGColor.init(srgbRed: 0.0, green: 0.588, blue: 0.796, alpha: 1.0)
         messageShape.layer.shadowOpacity = 0.55
         messageShape.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
         messageShape.layer.shadowRadius = CGFloat(5.0)
